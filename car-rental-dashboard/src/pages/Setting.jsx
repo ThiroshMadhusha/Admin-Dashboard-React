e829b346835554852cd43a6ad3c83704050e5e8b
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-10-09 14:04:38 +0530

    create setting page

---
+++ car-rental-dashboard/src/pages/Setting.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/setting.css";
+
+const Setting = () => {
+  return (
+    <div className="setting">
+      <div className="single__card">Setting</div>
+    </div>
+  );
+}
+
+export default Setting
